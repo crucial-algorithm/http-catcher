9891be179f28333bd9764cff13e35d0eaa9f780b
Filipe <kimile@gmail.com>
2024-03-22 11:46:58 +0000

    Support for storing messages in multiple buckets

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # http-catcher
 Simple http catcher created to support e2e tests
 
-Endpoints:
-|Method | URL | Description |
-|---|---|---|
-|POST | http://localhost:3001/ | Post any payload to be "stored" (previous payload will be overwritten) - tested with JSON payloads only |
-|GET |  http://localhost:3001/ | Read last message sent with POST |
-|GET |  http://localhost:3001/clear | Clear last message sent (if needed) |
+## Run in command line
+```bash
+npm run start
+```
 
-Test locally:
+## Adding new custom endpoints
+Edit file `custom-endpoints.json` and add new record to the array
+
+## Run docker image locally:
 ```shell
 docker build -t http-catcher .
 docker run http-catcher
 ```
 
 
-Build to push to docker hub:
+## Build to push to docker hub:
 ```shell
 # latest
 docker build --platform linux/amd64  -t filipeutter/http-catcher .
@@ -27,6 +28,8 @@ docker push filipeutter/http-catcher:2.1
 ```
 
 Define custom endpoints by setting `ENDPOINTS` enviornment variable
-```shell
+```bash
 export ENDPOINTS=[ { "method": "post", "endpoint": "/sibs", "response": {"formContext": "zzz", "transactionID": 123456}, "sideEffect": { "url": "http://localhost:3000/wh/sibs", "method": "post", "body": { "merchantTransactionId": "{{merchantTransactionId}}"} } } ]
 ```
+
+
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const SimpleScheduler = require('./SimpleScheduler');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const port = process.env.PORT || 3001;
 let endpoints = process.env.ENDPOINTS;
@@ -22,36 +23,95 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-let message = [];
+// Define the CORS options
+const corsOptions = {
+  credentials: true,
+  origin(req, callback) {
+    callback(null, true);
+  }
+};
 
+app.use(cors(corsOptions)); // Use the cors middleware with your options
+app.options('*', cors(corsOptions));
+
+let storage = {
+  __default__: []
+}
 
 app.get('/', (req, res) => {
-  res.json(message.pop());
+  let bucket = req.query.bucket || '__default__';
+  let log = req.query.log !== undefined;
+
+  process.stdout.write(`... GET to / ${bucket}`);
+  if (!storage[bucket]) {
+    process.stdout.write(' [not created yet]\n');
+    res.status(204).send('Bucket not found');
+  } else {
+    process.stdout.write(` [${Math.max(0,storage[bucket].length - 1)} record(s)]\n`);
+    const result = storage[bucket].pop();
+
+    if (log === true) {
+      console.log(result, typeof result);
+    }
+
+    res.json(result);
+    res.end();
+  }
 });
 
 app.get('/clear', (req, res) => {
-  message = [];
-  res.status(200).send('ok');
+  let bucket = req.query.bucket || '__default__';
+
+  console.log(`... GET to /clear [${bucket}]`);
+  if (!storage[bucket]) {
+    res.status(204).send('Bucket not found');
+  } else {
+    console.log(`... ${storage[bucket].length} to be discarded`);
+    storage[bucket] = [];
+    res.status(200).send('ok');
+  }
+  res.end();
+});
+
+
+app.get("/clear-all", (req, res) => {
+  console.log(`... GET to /clear-all`);
+
+  storage = {
+    __default__: [],
+  };
+  res.status(200).send("ok");
+  res.end();
 });
 
 app.get('/health-check', (req, res) => {
-  message = [];
   res.status(200).send(getRemoteAddress(req));
+  res.end();
 });
 
 
-app.post('/', (req, res) => {
-  message.push(req.body);
-  res.status(200).send('Stored');
+app.post("/", (req, res) => {
+  let bucket = req.query.bucket || "__default__";
+
+  process.stdout.write(`... POST to / ${bucket}`);
+  if (!storage[bucket]) {
+    storage[bucket] = [];
+  }
+
+  process.stdout.write(` [${storage[bucket].length + 1} record(s)] \n`);
+  storage[bucket].push(req.body);
+  res.status(200).send("Stored");
+  res.end();
 });
 
-console.log('... version 20230721-1404');
+console.log('... version 20240323-1213');
 
 if (endpoints.length > 0) console.log('... loading custom endpoints')
-endpoints.map(({method, endpoint, response, sideEffect}) => {
-  console.log(`    ${method.toUpperCase()} ${processEndpoint(endpoint)} ${sideEffect ? ' with side effect' : ''}`);
+endpoints.map(({method, endpoint, response, sideEffect, keep}) => {
+  console.log(`    ${method.toUpperCase()} ${processEndpoint(endpoint)} ${sideEffect ? ' with side effect' : ''}`, keep);
 
   app[method.toLowerCase()](processEndpoint(endpoint), multer().none(), async (req, res) => {
+    console.log('...', method.toUpperCase(), endpoint);
     // side effects are used to simulate making calls during request
     await handleSideEffect(sideEffect, req);
 
@@ -64,6 +124,7 @@ endpoints.map(({method, endpoint, response, sideEffect}) => {
     } else {
       res.json(JSON.parse(replaceVars(JSON.stringify(response), vars(req))));
     }
+    res.end();
   });
 })
 
@@ -103,6 +164,8 @@ const lookup = function recurse(array,object) {
 async function handleSideEffect(sideEffect, req) {
   if (!sideEffect) return;
 
+  console.log('... has side effect');
+
   const context = vars(req);
   const payload = replaceVars(JSON.stringify(sideEffect.body), context);
   const headers = { 'Content-Type': 'application/json' };
@@ -138,6 +201,7 @@ function vars(req) {
 }
 
 function handleRedirect(target, req, res) {
+  console.log('... handling redirect to', target);
   res.redirect(replaceVars(target, vars(req)));
 }
 
@@ -164,6 +228,8 @@ function handleCase(options, req, res) {
   let value;
 
   value = req.params[test] || replaceVars(test, vars(req));
+
+  console.log('... handling case with value', value);
   let response = (responses.find((r) => r.value === value) || { response: null }).response;
 
   if (!response && defaultOption) {
